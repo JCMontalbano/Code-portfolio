5e59ed0bcc9dfed32db9f4dba68cdaa3d3949b5f
John Montalbano <jcmontalbano@gmail.com>
2018-07-23 13:48:54 -0500

    Create TwitterClassifier.v.2.py
    Version 2 produced the neiltyson.SHAQ.07232018.h5 neural network. It takes 10 keywords now, and implements Vader sentiment analysis in the function SentimentAnalyze(tweet) to vectorize the tweets. 
    Next version needs counter-filtering between the keyword lists.

---
+++ TwitterClassifier/TwitterClassifier.v.2.py
@@ -0,0 +1,353 @@
+#This python script will pull tweets from two twitter accounts, which it accepts
+#as arguments, write a brief report on the results, and then trains a neural net
+#to distinguish the two.
+#sample command: python3 TwitterClassifier.py realDonaldTrump DrJillStein reportfile.txt
+import time
+import sys
+import pandas as pd
+#Twitter scrape:
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import tweepy
+#Natural Language Processing:
+from nltk import word_tokenize, sent_tokenize
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import string
+from collections import Counter
+#Neural networks:
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.utils import to_categorical
+import datetime
+
+#target1='realDonaldTrump'
+#target2='DrJillStein'
+#reportfile='reportfile.txt'
+target1=sys.argv[1]
+target2=sys.argv[2]
+reportfile=sys.argv[3]
+
+#Tweepy: collect sample tweets
+ckey ='K7fyRPBdo3lHqsydEQm1UI9J1'
+csecret = 'O4lldomxfzutFhgwlgjN01J8LaJvqhlYzjNt8Xr5kxKi85QpV2'
+atoken = '1531086302-yDaRtB2uFlxwqGK7qwzKNYQuQpkBqPfA2lX5s4R'
+asecret ='ajQ8gVN7pcs1pWyw2HjFwHQuow1NHdTji8iWypIVOKUWf'
+
+auth = tweepy.OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+api = tweepy.API(auth)
+
+def GetTweets(target):
+    tweetlist=[]
+    for tweet in tweepy.Cursor(api.user_timeline,screen_name=target,tweet_mode='extended').items():
+        tweetlist.append(tweet.full_text)
+        print(tweet.full_text)
+    return(tweetlist)
+
+print('Retrieving tweets for '+target1+' and '+target2)
+targetlist1=GetTweets(target1)
+targetlist2=GetTweets(target2)
+
+#NLTK: vectorize tweets to 21 variables:
+
+#count words and ID 10 most frequent non-stop words in each corpus
+print('Counting most frequent words...')
+
+bigString1=''
+for tweet in targetlist1:
+	bigString1=bigString1+' '+tweet
+
+bigString2=''
+for tweet in targetlist2:
+	bigString2=bigString2+' '+tweet
+#remove punctuation and numerals
+bigString1 = bigString1.translate(str.maketrans('','',string.punctuation))
+bigString1 = bigString1.translate(str.maketrans('','','1234567890'))
+bigString2 = bigString2.translate(str.maketrans('','',string.punctuation))
+bigString2 = bigString2.translate(str.maketrans('','','1234567890'))
+#note: you can distinguish iPhone from Android tweet sources by the presence of
+#fancy “quotes” which point inward, rather that "quotes" which are the same character.
+#change to the following filter string to eliminate them too: 
+#bigString1 = bigString1.translate(str.maketrans('','','1234567890“”’'))
+
+
+#tokenize
+bigtoken1=word_tokenize(bigString1);prefilter1=len(bigtoken1)
+bigtoken2=word_tokenize(bigString2);prefilter2=len(bigtoken2)
+#eliminate stopwords:
+from nltk.corpus import stopwords
+stop_words=set(stopwords.words('english'))
+bigtoken1=[w for w in bigtoken1 if not w in stop_words]
+bigtoken2=[w for w in bigtoken2 if not w in stop_words]
+nonstop1=str(len(bigtoken1)/prefilter1).ljust(4)[:4]
+nonstop2=str(len(bigtoken2)/prefilter2).ljust(4)[:4]
+
+print(target1+' is '+nonstop1+' unique words.')
+print(target2+' is '+nonstop2+' unique words.')
+
+#count them
+c1=Counter(bigtoken1)
+c1=c1.most_common(10)
+c2=Counter(bigtoken2)
+c2=c2.most_common(10)
+#problem here: many most-common words are turning up in both. Counter-filter down the lists to eliminate this problem.
+
+keywordlist=[]
+keywordlist1=[]
+keywordlist2=[]
+for count in c1:
+	keywordlist.append(count[0])
+	keywordlist1.append(count[0])
+
+for count in c2:
+	keywordlist.append(count[0])
+	keywordlist2.append(count[0])
+
+#Add 10 top words for each target to the report
+print('@'+target1+' most common words:'+str(keywordlist1))
+print('@'+target2+' most common words:'+str(keywordlist2))
+open(reportfile,'a').write(target1+' most common words:'+str(keywordlist1)+'\n')
+open(reportfile,'a').write(target2+' most common words:'+str(keywordlist2)+'\n')
+
+sid = SentimentIntensityAnalyzer()
+def SentimentAnalyze(tweet):
+	sentences=sent_tokenize(tweet)
+	neulist=[];poslist=[];neglist=[];comlist=[]
+	for sentence in sentences:
+		ss = sid.polarity_scores(sentence)
+		neulist.append(ss['neu'])
+		poslist.append(ss['pos'])
+		neglist.append(ss['neg'])
+		comlist.append(ss['compound'])
+	#get average values
+	neu=sum(neulist)/len(neulist)
+	pos=sum(poslist)/len(poslist)
+	neg=sum(neglist)/len(neglist)
+	compound=sum(comlist)/len(comlist)
+	return(neg,pos,neu,compound)
+
+
+#for each tweet in target1, then in target2:
+VectorDF=pd.DataFrame(columns=['neg','pos','neu','compound','k0','k1','k2','k3','k4','k5','k6','k7','k8','k9','k10','k11','k12','k13','k14','k15','k16','k17','k18','k19','AvgLength','Target1'])
+for tweet in targetlist1:
+	row=[]
+	neg,pos,neu,compound=SentimentAnalyze(tweet)
+	wtoken=word_tokenize(tweet)
+	key0=(wtoken.count(keywordlist[0]))
+	key1=(wtoken.count(keywordlist[1]))
+	key2=(wtoken.count(keywordlist[2]))
+	key3=(wtoken.count(keywordlist[3]))
+	key4=(wtoken.count(keywordlist[4]))
+	key5=(wtoken.count(keywordlist[5]))
+	key6=(wtoken.count(keywordlist[6]))
+	key7=(wtoken.count(keywordlist[7]))
+	key8=(wtoken.count(keywordlist[8]))
+	key9=(wtoken.count(keywordlist[9]))
+	key10=(wtoken.count(keywordlist[10]))
+	key11=(wtoken.count(keywordlist[11]))
+	key12=(wtoken.count(keywordlist[12]))
+	key13=(wtoken.count(keywordlist[13]))
+	key14=(wtoken.count(keywordlist[14]))
+	key15=(wtoken.count(keywordlist[15]))
+	key16=(wtoken.count(keywordlist[16]))
+	key17=(wtoken.count(keywordlist[17]))
+	key18=(wtoken.count(keywordlist[18]))
+	key19=(wtoken.count(keywordlist[19]))
+	AvgLength= len(word_tokenize(tweet))/len(sent_tokenize(tweet)) #average sentence length, in words
+	row=[neg,pos,neu,compound,key0,key1,key2,key3,key4,key5,key6,key7,key8,key9,
+	key10,key11,key12,key13,key14,key15,key16,key17,key18,key19,AvgLength,1]
+	print(row)
+	VectorDF.loc[len(VectorDF)]=row
+
+for tweet in targetlist2:
+	row=[]
+	neg,pos,neu,compound=SentimentAnalyze(tweet)
+	wtoken=word_tokenize(tweet)
+	key0=(wtoken.count(keywordlist[0]))
+	key1=(wtoken.count(keywordlist[1]))
+	key2=(wtoken.count(keywordlist[2]))
+	key3=(wtoken.count(keywordlist[3]))
+	key4=(wtoken.count(keywordlist[4]))
+	key5=(wtoken.count(keywordlist[5]))
+	key6=(wtoken.count(keywordlist[6]))
+	key7=(wtoken.count(keywordlist[7]))
+	key8=(wtoken.count(keywordlist[8]))
+	key9=(wtoken.count(keywordlist[9]))
+	key10=(wtoken.count(keywordlist[10]))
+	key11=(wtoken.count(keywordlist[11]))
+	key12=(wtoken.count(keywordlist[12]))
+	key13=(wtoken.count(keywordlist[13]))
+	key14=(wtoken.count(keywordlist[14]))
+	key15=(wtoken.count(keywordlist[15]))
+	key16=(wtoken.count(keywordlist[16]))
+	key17=(wtoken.count(keywordlist[17]))
+	key18=(wtoken.count(keywordlist[18]))
+	key19=(wtoken.count(keywordlist[19]))
+	AvgLength= len(word_tokenize(tweet))/len(sent_tokenize(tweet)) #average sentence length, in words
+	row=[neg,pos,neu,compound,key0,key1,key2,key3,key4,key5,key6,key7,key8,key9,
+	key10,key11,key12,key13,key14,key15,key16,key17,key18,key19,AvgLength,0]
+	print(row)
+	VectorDF.loc[len(VectorDF)]=row
+
+#TensorFlow: 
+#load NLTK vectors as a target array
+#select random 20% of array, 50% class balanced, load it as training data
+#train a network using parameters until you get a target threshold
+
+#save vectorized DF
+#generate a dynamic DF name:
+now=datetime.datetime.now().strftime('%m%d%Y')
+DF_name=target1+'.'+target2+'.'+str(now)+'.vectorDF.csv'
+VectorDF.to_csv(DF_name) #save VectorDF
+DF=VectorDF
+targetcolumn='Target1'
+
+#These parameters were useful for this approach in a different problem, but they don't
+#appear very good, for this problem. These parameters need some work.
+ratio=0.2 #we'll select 20% of the dataset
+layerzlist=[20,22,25,28] #these need to hold the series of parameters we know tend to find solutions
+nodezlist=[200,200,250] #4 and 3 are mutually prime, so it'll exhaustively hit all the possible combos
+epochs=12
+activator='relu'
+losser='categorical_crossentropy' #these last 2 are hardcoded for now but can easily be changed
+
+#we set the accuracy targets for this approach by 
+targetacc=0.75
+minacc=0.6
+
+#function which selects a percent
+def ClassBalance(TargetDF, ratio):
+	targetDF1=TargetDF[TargetDF['Target1']==1]
+	targetDF2=TargetDF[TargetDF['Target1']==0]
+	n=int(len(TargetDF)*(ratio/2))
+	targetDF1=targetDF1.sample(n=n)
+	targetDF2=targetDF2.sample(n=n)
+	returnDF=pd.concat([targetDF1,targetDF2], axis=0)
+	returnDF=returnDF.sample(frac=1).reset_index(drop=True) #drop=True prevents .reset_index from creating a col with the old index entries
+	return(returnDF)
+
+def TrainNet(TargetDF, layerz, nodez, epochs, activator, losser):
+	target=to_categorical(TargetDF[targetcolumn])
+	predictors=TargetDF.drop([targetcolumn], axis=1).as_matrix()
+	n_cols=predictors.shape[1]
+	model=Sequential()
+	model.add(Dense(nodez,activation=activator,input_shape=(n_cols,)))
+	for layer in range(0,layerz):
+		model.add(Dense(nodez,activation=activator))
+	model.add(Dense(2,activation='softmax'))
+	model.compile(optimizer='adam',loss=losser, metrics=['accuracy'])
+	model_training=model.fit(predictors,target, epochs=epochs) #
+	acc=model_training.history['acc'] #how do i get just the last value?
+	loss=model_training.history['loss']
+	acc=acc[-1]
+	return(model,acc) 
+
+#generate a dynamic neural network name:
+now=datetime.datetime.now().strftime('%m%d%Y')
+net_name=target1+'.'+target2+'.'+str(now)+'.h5'
+
+print('Good luck!')
+trainDF=ClassBalance(VectorDF,ratio) #class balance it, instance a new DF for this run
+dropratio=0
+new_net=False
+
+while new_net==False:
+	for layerz in layerzlist:
+		for nodez in nodezlist:#will these lists cycle correctly? Yes
+			model,acc=TrainNet(trainDF, layerz, nodez, epochs, activator, losser)
+			if acc>(targetacc-dropratio): #if acc>target, name and save it and new_net=Tru
+				new_net=True
+				print('We did it!')
+				model.save(net_name)
+				open(reportfile,'a').write('\nSaved '+net_name+' ')
+				now=datetime.datetime.now().strftime('%m/%d/%y %H:%M')
+				open(reportfile,'a').write(str(now)+'\t Accuracy:'+str(acc)+'\t'+'Layers:'+str(layerz)+' Nodes:'+str(nodez)+' DF length:'+str(len(trainDF)))
+			else: 
+				del model
+				trainDF=ClassBalance(VectorDF,ratio) #resample it
+				trainDF=trainDF.sample(frac=1)
+				if (targetacc-dropratio)>minacc:
+					dropratio+=0.001
+				print('Nope, dropratio is '+str(dropratio)+', new acc target is '+str(targetacc-dropratio))
+				print('Layers:'+str(layerz)+' Nodes:'+str(nodez)+' trainDF length:'+str(len(trainDF)))
+			if new_net==True:
+				break
+		if new_net==True:
+			break
+
+
+donetime=datetime.datetime.now().strftime('%d/%m/%y %H:%M')
+print('Done at '+str(donetime)+' :D')
+
+
+#After this script runs, you'll have a custom-named neural network.
+#You'll then need a separate script to test that network, which will do the following:
+
+#give the network the remaining 80% of NLTK vectors to predict on
+
+#compare the network's predictions to the actuals:
+	#take the higher value as its prediction
+	#go through the 'target1' preds, if>0.5 pred=1, if<0.5 pred=0
+	#go through the rows side by side, if target1 = target1 pred, True
+	#take the % True as score
+
+#Append % True score to end of report
+
+
+'''
+#this is the function to Vectorize tweets, it needs a bit more work to replace the old code.
+#Presently I have this code duplicated above, because it was easy to cross-stitch the data 
+#objects as they were produced.
+def VectorizeTweets(target,targetlist):
+	bigString=''
+	for tweet in targetlist:
+		bigString=bigString+' '+tweet
+	bigString = bigString.translate(str.maketrans('','',string.punctuation))
+	bigString = bigString.translate(str.maketrans('','','1234567890’“”'))
+	bigtoken=word_tokenize(bigString);prefilter=len(bigtoken)
+	bigtoken=[w for w in bigtoken if not w in stop_words]
+	nonstop=str(len(bigtoken)/prefilter).ljust(4)[:4]
+	print(target1+' is '+nonstop+' unique words.')
+	c=Counter(bigtoken)
+	c=c.most_common(10)
+	keywordlist=[]
+	for count in c1:
+	keywordlist.append(count[0])
+	print(target+' most common words:'+str(keywordlist))
+	open(reportfile,'a').write(target+' most common words:'+str(keywordlist)+'\n')	
+	#for each tweet in target1, then in target2:
+	VectorDF=pd.DataFrame(columns=['k0','k1','k2','k3','k4','k5','k6','k7','k8','k9','k10','k11','k12','k13','k14','k15','k16','k17','k18','k19','AvgLength','Target1'])
+	for tweet in targetlist1:
+		row=[]
+		neg,pos,neu,compound=SentimentAnalyze(tweet)
+		wtoken=word_tokenize(tweet)
+		key0=(wtoken.count(keywordlist[0]))
+		key1=(wtoken.count(keywordlist[1]))
+		key2=(wtoken.count(keywordlist[2]))
+		key3=(wtoken.count(keywordlist[3]))
+		key4=(wtoken.count(keywordlist[4]))
+		key5=(wtoken.count(keywordlist[5]))
+		key6=(wtoken.count(keywordlist[6]))
+		key7=(wtoken.count(keywordlist[7]))
+		key8=(wtoken.count(keywordlist[8]))
+		key9=(wtoken.count(keywordlist[9]))
+		key10=(wtoken.count(keywordlist[10]))
+		key11=(wtoken.count(keywordlist[11]))
+		key12=(wtoken.count(keywordlist[12]))
+		key13=(wtoken.count(keywordlist[13]))
+		key14=(wtoken.count(keywordlist[14]))
+		key15=(wtoken.count(keywordlist[15]))
+		key16=(wtoken.count(keywordlist[16]))
+		key17=(wtoken.count(keywordlist[17]))
+		key18=(wtoken.count(keywordlist[18]))
+		key19=(wtoken.count(keywordlist[19]))
+		AvgLength= len(word_tokenize(tweet))/len(sent_tokenize(tweet)) #average sentence length, in words
+		row=[key0,key1,key2,key3,key4,key5,key6,key7,key8,key9,
+		key10,key11,key12,key13,key14,key15,key16,key17,key18,key19,AvgLength,1]
+		print(row)
+		VectorDF.loc[len(VectorDF)]=row
+	return VectorDF, keywordlist
+'''
